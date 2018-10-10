a1d417e26c50a758f863839e7033f4db7437b8e9
diekus <diekus@gmail.com>
2018-10-10 13:39:06 +0100

    first commit

---
+++ scripts/webxrbt.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function() {
+  let canvas = document.getElementById('renderCanvas');
+
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  let engine = new BABYLON.Engine(canvas, true);
+
+  let createScene = function(){
+    let scene = new BABYLON.Scene(engine);
+    var camera = new BABYLON.WebVRFreeCamera("camera1", new BABYLON.Vector3(0, 0, 0), scene);
+    camera.attachControl(canvas, true);
+
+    scene.onPointerDown = function () {
+      scene.onPointerDown = undefined
+      camera.attachControl(canvas, true);
+    }
+
+    // Add lights to the scene
+    var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
+    var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
+
+    // Add and manipulate meshes in the scene
+    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter:2}, scene);
+    sphere.position = new BABYLON.Vector3(0,0, 2)
+
+    return scene;
+
+
+  };
+
+  let scene = createScene();
+  //registers a render loop
+  engine.runRenderLoop(function(){
+    scene.render();
+  });
+
+  //handles resize event
+  window.addEventListener("resize", function(){
+    engine.resize();
+  })
+
+});
+
+
+
